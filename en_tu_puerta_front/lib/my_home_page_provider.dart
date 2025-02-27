35c64733f09aa74a8c22ad6c49155748888f739b
Daniela Zambrano <danazambranoleon@gmail.com>
2025-02-27 19:40:07 -0400

    Refactorización
    Cambio de nombre home_provider_screen

--- en_tu_puerta_front/lib/my_home_page_provider.dart
+++ en_tu_puerta_front/lib/my_home_page_provider.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:en_tu_puerta_front/widget_provider/screen_provider_home.dart';
+import 'package:en_tu_puerta_front/widget_provider/home_provider_screen.dart';
 import 'package:en_tu_puerta_front/widget_provider/petitions_provider_screen.dart';
 import 'package:en_tu_puerta_front/widget_provider/screen_provider_metrics.dart';
 import 'package:en_tu_puerta_front/widget_provider/settings_provider_screen.dart';
