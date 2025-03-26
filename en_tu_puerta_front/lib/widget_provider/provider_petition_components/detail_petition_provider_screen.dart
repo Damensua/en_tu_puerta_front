b659a52510642bf8ba4da51470a8c7c36b11b415
Diego Mendez <d.mendez@correo.unimet.edu.ve>
2025-03-26 00:25:28 -0400

    Detalles de la peticion arreglados

--- en_tu_puerta_front/lib/widget_provider/provider_petition_components/detail_petition_provider_screen.dart
+++ en_tu_puerta_front/lib/widget_provider/provider_petition_components/detail_petition_provider_screen.dart
@@ -29,22 +29,33 @@ class _DetailPetitionProviderScreenState
         context: context,
         builder: (context) => AlertDialog(
           title: Text('Solicitud aceptada!'),
+
           content: Column(
             mainAxisSize: MainAxisSize.min,
             children: [
+              SizedBox(height: 10),
               Text('Se ha aceptado correctamente la solicitud.'),
               SizedBox(height: 20),
               SizedBox(
                 width: double.infinity,
-                child: ReusableButton(
-                  text: 'Aceptar',
-                  color: Color(0xFF001563),
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                  padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+                  backgroundColor: Color(0xFF001563),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                  ),
                   onPressed: () {
-                    Navigator.pop(context);
-                    onAccept();
+                  Navigator.pop(context);
+                  onAccept();
                   },
+                  child: Text(
+                  'Aceptar',
+                  style: TextStyle(fontSize: 14, color: Colors.white),
+                  ),
                 ),
-              ),
+                ),
             ],
           ),
         ),
@@ -69,83 +80,109 @@ class _DetailPetitionProviderScreenState
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             // Avatar del usuario
-            CircleAvatar(
-              radius: 60,
-              backgroundImage: const NetworkImage(
-                "https://previews.123rf.com/images/john79/john791708/john79170800031/84347480-chicas-de-perfil-para-sal%C3%B3n-de-belleza-y-peluquer%C3%ADa-con-tijeras-y-peine.jpg",
+            Center(
+              child: CircleAvatar(
+              radius: 90,
+              backgroundImage: NetworkImage(
+                widget.petition.imageUser?.replaceAll('"', '') ?? 'https://i.postimg.cc/05h66XrJ/Artboard-1-copy-2-3x.png',
+              ),
               ),
             ),
             const SizedBox(height: 16),
 
             // Nombre del usuario
-            Text(
-              '${widget.petition.firstNameUser ?? 'Usuario'} ${widget.petition.lastNameUser ?? ''}',
-              style: const TextStyle(
-                fontSize: 24,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            const SizedBox(height: 8),
+            Padding(
+              padding: const EdgeInsets.only(left: 16.0),
+              child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                '${widget.petition.firstNameUser ?? 'Usuario'} ${widget.petition.lastNameUser ?? ''}',
+                style: const TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                ),
+                ),
+                const SizedBox(height: 8),
 
-            // Estado de la solicitud
-            Text(
-              'Estado: ${widget.petition.status ?? 'Desconocido'}',
-              style: const TextStyle(
-                fontSize: 18,
-                color: Colors.blueGrey,
-              ),
-            ),
-            const SizedBox(height: 16),
+                // Estado de la solicitud
+                Text(
+                'Estado: ${widget.petition.status ?? 'Desconocido'}',
+                style: const TextStyle(
+                  fontSize: 18,
+                  color: Colors.blueGrey,
+                ),
+                ),
+                const SizedBox(height: 16),
 
-            // Información del servicio
-            const Text(
-              'Servicio solicitado:',
-              style: TextStyle(
-                fontSize: 18,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            Text(
-              widget.petition.nameService ?? 'No especificado',
-              style: const TextStyle(fontSize: 16),
-            ),
-            const SizedBox(height: 16),
+                // Información del servicio
+                const Text(
+                'Servicio solicitado:',
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                ),
+                ),
+                Text(
+                widget.petition.nameService ?? 'No especificado',
+                style: const TextStyle(fontSize: 16),
+                ),
+                const SizedBox(height: 16),
 
-            // Fecha y hora de la solicitud
-            const Text(
-              'Fecha y hora:',
-              style: TextStyle(
-                fontSize: 18,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            Text(
-              '${widget.petition.date} - ${widget.petition.time}',
-              style: const TextStyle(fontSize: 16),
-            ),
-            const SizedBox(height: 16),
+                // Fecha y hora de la solicitud
+                const Text(
+                'Fecha:',
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                ),
+                ),
+                Text(
+                widget.petition.date,
+                style: const TextStyle(fontSize: 16),
+                ),
+                const SizedBox(height: 8),
+                const Text(
+                'Hora:',
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                ),
+                ),
+                Text(
+                widget.petition.time ?? 'No especificada',
+                style: const TextStyle(fontSize: 16),
+                ),
+                const SizedBox(height: 16),
 
-            // Mensaje del usuario
-            const Text(
-              'Mensaje:',
-              style: TextStyle(
-                fontSize: 18,
-                fontWeight: FontWeight.bold,
+                // Mensaje del usuario
+                const Text(
+                'Mensaje:',
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                ),
+                ),
+                Text(
+                widget.petition.message ?? 'Sin mensaje',
+                style: const TextStyle(fontSize: 16),
+                ),
+              ],
               ),
             ),
-            Text(
-              widget.petition.message ?? 'Sin mensaje',
-              style: const TextStyle(fontSize: 16),
-            ),
-            SizedBox(height: 16),
-            ReusableButton(
+            const SizedBox(height: 90),
+
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: ReusableButton(
               text: 'Aceptar solicitud',
               color: const Color(0xFF001563),
               onPressed: () {
                 _acceptRequest(widget.petition, widget.onAccept);
               },
+              ),
             ),
-          ],
+            ],
         ),
       ),
     );
